4fa71391c4927033d76c526892d5bd72ae38688d
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2023-02-26 20:22:59 +0900

    Update ab01nd.py
    [Regenerate response]

--- src/ab01nd.py
+++ src/ab01nd.py
@@ -1,80 +1,74 @@
 import numpy as np
-from scipy.linalg import blas
+from scipy.linalg import (blas, lapack)
 
-def ab01nd(jobz, n, m, a, lda, b, ldb, ncont, indcon, nblk, z, ldz, tau, tol, iwork, dwork, ldwork):
-    zero, one = 0.0, 1.0
+def ab01nd(jobz, n, m, a, lda, b, ldb, tol=0.0):
+    zero = np.float64(0.0)
+    one = np.float64(1.0)
 
-    # Parameters
-    def lsame(a, b):
-        return a.lower() == b.lower()
-
-    def dlange(norm, m, n, a, lda, work):
-        return blas.dlange(norm, m, n, a, lda)
-
-    def dlamch(c):
-        return blas.dlamch(c)
-
-    def xerbla(routine, info):
-        raise ValueError(f"Routine {routine} called with invalid input: info = {info}")
-
-    # Local variables
-    ljobf = lsame(jobz, 'F')
-    ljobi = lsame(jobz, 'I')
-    ljobz = ljobf or ljobi
-
-    # Test the input scalar arguments
-    info = 0
-    if not ljobz and not lsame(jobz, 'N'):
-        info = -1
-    elif n < 0:
-        info = -2
-    elif m < 0:
-        info = -3
-    elif lda < max(1, n):
-        info = -5
-    elif ldb < max(1, n):
-        info = -7
-    elif ldz < 1 or (ljobz and ldz < n):
-        info = -12
-    elif ldwork < max(1, n, 3*m):
-        info = -17
-
-    if info != 0:
-        xerbla('AB01ND', -info)
-        return
+    # Test the input scalar arguments.
+    if jobz not in ['N', 'F', 'I']:
+        raise ValueError("JOBZ must be 'N', 'F', or 'I'")
+    if n < 0:
+        raise ValueError("N must be non-negative")
+    if m < 0:
+        raise ValueError("M must be non-negative")
+    if lda < max(1, n):
+        raise ValueError("LDA must be at least max(1, N)")
+    if ldb < max(1, n):
+        raise ValueError("LDB must be at least max(1, N)")
 
     ncont = 0
     indcon = 0
+    ljobf = jobz in ['F']
+    ljobi = jobz in ['I']
+    ljobz = ljobf or ljobi
 
-    # Quick return if possible
+    # Quick return if possible.
     if min(n, m) == 0:
         if n > 0:
+            z = np.zeros((n, n))
+            tau = np.zeros(n)
             if ljobi:
-                z[:n, :n] = np.eye(n)
-            elif ljobf:
-                z[:n, :n] = np.zeros((n, n))
-                tau[:n] = 0
-        dwork[0] = one
-        return
+                np.fill_diagonal(z, 1.0)
+            dwork = np.array([one], dtype=np.float64)
+        else:
+            z = np.empty((0,0), dtype=np.float64)
+            tau = np.empty(0, dtype=np.float64)
+            dwork = np.array([one], dtype=np.float64)
+        return z, tau, dwork
 
-    # Calculate the absolute norms of A and B (used for scaling)
-    anorm = dlange('M', n, n, a, lda, dwork)
-    bnorm = dlange('M', n, m, b, ldb, dwork)
+    # Calculate the absolute norms of A and B (used for scaling).
+    anorm = blas.dlange('M', n, n, a, lda)
+    bnorm = blas.dlange('M', n, m, b, ldb)
 
-    # Return if matrix B is zero
+    # Return if matrix B is zero.
     if bnorm == zero:
+        z = np.zeros((n, n))
+        tau = np.zeros(n)
         if ljobi:
-            z[:n, :n] = np.eye(n)
-        elif ljobf:
-            z[:n, :n] = np.zeros((n, n))
-            tau[:n] = 0
-        dwork[0] = one
-        return
+            np.fill_diagonal(z, 1.0)
+        dwork = np.array([one], dtype=np.float64)
+        return z, tau, dwork
 
-    # Scale (if needed) the matrices A and B
-    mb01pd = lambda job, scl, m, n, p, q, s, k, nb, a, lda, info: None
-    mb01pd('Scale', 'G', n, n, 0, 0, anorm, 0, nblk, a, lda, info)
-    mb01pd('Scale', 'G', n, m, 0, 0, bnorm, 0, nblk, b, ldb, info)
+    # Scale (if needed) the matrices A and B.
+    nblk = np.zeros(n, dtype=np.int32)
+    info = np.zeros(1, dtype=np.int32)
+    lapack.mb01pd('Scale', 'G', n, n, 0, 0, anorm, 0, nblk, a, lda, info)
+    lapack.mb01pd('Scale', 'G', n, m, 0, 0, bnorm, 0, nblk, b, ldb, info)
 
-    # Compute the Frobenius norm of [B A] (used for rank estimation)
-    fnrm = dlange('F',
+    # Compute the Frobenius norm of [B A] (used for rank estimation).
+    fnrm = blas.dlange('F', n, m, b, ldb)
+
+    toldef = tol
+    if toldef <= zero:
+        toldef = np.float64(n*n) * lapack.dlamch('Precision')
+
+    wrkopt = np.int32(1)
+    ni = np.int32(0)
+    itau = np.int32(0)
+    ncrt = np.int32(n)
+    mcrt = np.int32(m)
+    iqr = np.int32(0)
+
+    while True:
+        # Rank-revealing QR decomposition with column pivoting.
